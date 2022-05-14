9409278be678c2d36e3071b5a5546292bc444901
vcgmuse <59248189+vcgmuse@users.noreply.github.com>
2022-05-14 15:30:47 -0500

    Add files via upload

---
+++ FactorListMaker.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FactorListMaker {
+
+    public static List<Integer> determineInteractions(List<Integer> integerList) {
+        List<Integer> answer = new ArrayList<>();
+        for (Integer num : integerList) {
+            int factor = 5;
+            while (factor == 4 || num % factor != 0)
+                factor--;
+            System.out.println(getFactorList(num, factor));
+            System.out.println(getFactorList(num, factor).size());
+        }
+        return answer;
+    }
+
+    private static List<Integer> getFactorList(int value, int factor) {
+        List<Integer> tempList = new ArrayList<>();
+        if (factor == 1) {
+            tempList.add(factor);
+            tempList.add(value);
+            return tempList;
+        }
+
+        for (int i = factor; i <= value; i += factor) {
+            if (value % i == 0) {
+                if (!tempList.contains(i)) {
+                    tempList.add(i);
+                }
+                if (!tempList.contains(value / i)) {
+                    tempList.add(value / i);
+                }
+            }
+        }
+        return tempList;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // write your code here
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Please enter a number to determine the factors.");
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(25);
+        arrayList.add(6);
+        arrayList.add(2350);
+        arrayList.add(2349);
+        arrayList.add(2348);
+        arrayList.add(2347);
+        arrayList.add(872324);
+        FactorListMaker.determineInteractions(arrayList);
+    }
+}
+
